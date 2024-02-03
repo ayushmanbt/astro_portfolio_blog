66c6f14cf2e79cf80ef1d44a4d149d30188eb3c2
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2024-02-02 19:11:29 -0700

    new changes

--- src/scripts/LightDarkDetector.ts
+++ src/scripts/LightDarkDetector.ts
@@ -1,53 +1,112 @@
 const body = document.querySelector('body');
 const changer = document.getElementById('changer');
-const img = changer?.querySelector("img");
+let isSystemMode = false;
+const lightButton = document.getElementById("light_button");
+const darkButton = document.getElementById("dark_button");
+const systemButton = document.getElementById("system_button");
+const dialog = document.getElementById("menu");
+
+let el;
 
 const FromLightToDark = () => {
-
     if(!body) return
-
     body.classList.remove('light');
     body.classList.add('dark');
-    window.localStorage.setItem("mode", "dark");
-
-    if(img) img.src = "/icons/light.svg";
-
 }
 
 const FromDarkToLight = () => {
-
     if(!body) return
-
     body.classList.remove('dark');
     body.classList.add('light');
-    window.localStorage.setItem("mode", "light");
-
-    if(img) img.src = "/icons/dark.svg";
 }
 
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    FromLightToDark();
+const setButtons = ()=>{
+    if(darkButton && lightButton && systemButton){
+        if(isSystemMode) {
+            systemButton.style.display = 'none';
+            darkButton.style.display = 'block';
+            lightButton.style.display = 'block';
+        }
+        else if(body?.classList.contains('light')) {
+            lightButton.style.display = 'none';
+            darkButton.style.display = 'block';
+            systemButton.style.display = 'block';
+        }
+        else if(body?.classList.contains('dark')) {
+            darkButton.style.display = 'none';
+            lightButton.style.display = 'block';
+            systemButton.style.display = 'block';
+        }
+        
+    }    
+}    
+
+
+const FromSystem = () => {
+    isSystemMode = true;
+    window.localStorage.setItem("mode", "system");
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        FromLightToDark();
+    }
+    else if(window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+        FromDarkToLight();
+    }
+    
+    el = window.matchMedia('(prefers-color-scheme: dark)')
+          .addEventListener('change',({ matches }) => {
+      if (matches) {
+        FromDarkToLight();
+      } else {
+        FromLightToDark();
+      }
+    })
+
+    setButtons();
+    dialog?.close();
 }
-
-
-window.matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change',({ matches }) => {
-  if (matches) {
-    FromDarkToLight();
-  } else {
-    FromLightToDark();
-  }
-})
 
 function changemode () {
     if(!body) return
-
+    isSystemMode = false; 
     if(body.classList.contains('light')){
         FromLightToDark();
-        return;
+        window.localStorage.setItem("mode", "dark");
     }
-    FromDarkToLight();
+    else{
+        FromDarkToLight();
+        window.localStorage.setItem("mode", "light");
+    }
+    setButtons();
+    if(el) window.matchMedia('(prefers-color-scheme: dark)').removeEventListener("change", el);
+    dialog?.close();
 }
 
-changer?.addEventListener("click", changemode);
+// changer?.addEventListener("click", changemode);
+
+
+
+changer?.addEventListener("click", () => {
+    dialog.showModal()
+    dialog?.classList.add("scaleup");
+    dialog?.addEventListener("animationend", function(){
+        dialog.classList.remove("scaleup");
+    })
+});
+
+lightButton?.addEventListener("click", changemode);
+darkButton?.addEventListener("click", changemode);
+systemButton?.addEventListener("click", FromSystem);
+
+if(window.localStorage.getItem("mode") == "light"){
+    console.log("Why did I do this?");
+}
+else if(window.localStorage.getItem("mode") == "dark"){
+    changemode();
+}
+else{
+    FromSystem();
+    isSystemMode = true;
+}
+
+setButtons();
