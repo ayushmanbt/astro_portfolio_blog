3d22c98b1398579a6abed04c19cdd696af525dfa
ayushmanbt <ayushmanbilasthakur@gmal.com>
2024-05-11 02:11:34 -0700

    Resume update and minor bug fix

--- src/scripts/LightDarkDetector.ts
+++ src/scripts/LightDarkDetector.ts
@@ -1,112 +1,116 @@
-const body = document.querySelector('body');
-const changer = document.getElementById('changer');
+const body = document.querySelector("body");
+const changer = document.getElementById("changer");
 let isSystemMode = false;
 const lightButton = document.getElementById("light_button");
 const darkButton = document.getElementById("dark_button");
 const systemButton = document.getElementById("system_button");
-const dialog = document.getElementById("menu");
+const dialog = <HTMLDialogElement>document.getElementById("menu");
+const closeButton = <HTMLButtonElement>document.getElementById("close_button");
 
-let el;
+let el: any;
 
 const FromLightToDark = () => {
-    if(!body) return
-    body.classList.remove('light');
-    body.classList.add('dark');
-}
+  if (!body) return;
+  body.classList.remove("light");
+  body.classList.add("dark");
+};
 
 const FromDarkToLight = () => {
-    if(!body) return
-    body.classList.remove('dark');
-    body.classList.add('light');
-}
+  if (!body) return;
+  body.classList.remove("dark");
+  body.classList.add("light");
+};
 
-
-const setButtons = ()=>{
-    if(darkButton && lightButton && systemButton){
-        if(isSystemMode) {
-            systemButton.style.display = 'none';
-            darkButton.style.display = 'block';
-            lightButton.style.display = 'block';
-        }
-        else if(body?.classList.contains('light')) {
-            lightButton.style.display = 'none';
-            darkButton.style.display = 'block';
-            systemButton.style.display = 'block';
-        }
-        else if(body?.classList.contains('dark')) {
-            darkButton.style.display = 'none';
-            lightButton.style.display = 'block';
-            systemButton.style.display = 'block';
-        }
-        
-    }    
-}    
-
+const setButtons = () => {
+  if (darkButton && lightButton && systemButton) {
+    if (isSystemMode) {
+      systemButton.style.display = "none";
+      darkButton.style.display = "block";
+      lightButton.style.display = "block";
+    } else if (body?.classList.contains("light")) {
+      lightButton.style.display = "none";
+      darkButton.style.display = "block";
+      systemButton.style.display = "block";
+    } else if (body?.classList.contains("dark")) {
+      darkButton.style.display = "none";
+      lightButton.style.display = "block";
+      systemButton.style.display = "block";
+    }
+  }
+};
 
 const FromSystem = () => {
-    isSystemMode = true;
-    window.localStorage.setItem("mode", "system");
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        FromLightToDark();
-    }
-    else if(window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-        FromDarkToLight();
-    }
-    
-    el = window.matchMedia('(prefers-color-scheme: dark)')
-          .addEventListener('change',({ matches }) => {
+  isSystemMode = true;
+  window.localStorage.setItem("mode", "system");
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    FromLightToDark();
+  } else if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: light)").matches
+  ) {
+    FromDarkToLight();
+  }
+
+  el = window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", ({ matches }) => {
       if (matches) {
         FromDarkToLight();
       } else {
         FromLightToDark();
       }
-    })
+    });
 
-    setButtons();
-    dialog?.close();
-}
+  setButtons();
+  dialog?.close();
+};
 
-function changemode () {
-    if(!body) return
-    isSystemMode = false; 
-    if(body.classList.contains('light')){
-        FromLightToDark();
-        window.localStorage.setItem("mode", "dark");
-    }
-    else{
-        FromDarkToLight();
-        window.localStorage.setItem("mode", "light");
-    }
-    setButtons();
-    if(el) window.matchMedia('(prefers-color-scheme: dark)').removeEventListener("change", el);
-    dialog?.close();
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+function changemode() {
+  if (!body) return;
+  isSystemMode = false;
+  if (body.classList.contains("light")) {
+    FromLightToDark();
+    window.localStorage.setItem("mode", "dark");
+  } else {
+    FromDarkToLight();
+    window.localStorage.setItem("mode", "light");
+  }
+  setButtons();
+  if (el)
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .removeEventListener("change", el);
+  dialog?.close();
 }
 
 // changer?.addEventListener("click", changemode);
 
-
-
 changer?.addEventListener("click", () => {
-    dialog.showModal()
-    dialog?.classList.add("scaleup");
-    dialog?.addEventListener("animationend", function(){
-        dialog.classList.remove("scaleup");
-    })
+  dialog.showModal();
+  dialog?.classList.add("scaleup");
+  dialog?.addEventListener("animationend", function() {
+    dialog.classList.remove("scaleup");
+  });
 });
 
 lightButton?.addEventListener("click", changemode);
 darkButton?.addEventListener("click", changemode);
 systemButton?.addEventListener("click", FromSystem);
 
-if(window.localStorage.getItem("mode") == "light"){
-    console.log("Why did I do this?");
-}
-else if(window.localStorage.getItem("mode") == "dark"){
-    changemode();
-}
-else{
-    FromSystem();
-    isSystemMode = true;
+if (window.localStorage.getItem("mode") == "light") {
+  console.log("Why did I do this?");
+} else if (window.localStorage.getItem("mode") == "dark") {
+  changemode();
+} else {
+  FromSystem();
+  isSystemMode = true;
 }
 
 setButtons();
--- src/scripts/NameAnimation.ts
+++ src/scripts/NameAnimation.ts
@@ -5,85 +5,80 @@ const nameHolder = document.getElementById("myname");
 const height = nameHolder?.getBoundingClientRect().height;
 
 //fix its height
-if(nameHolder){
-    nameHolder.style.overflow = "hidden"
-    nameHolder.style.height = `${height}px`;
+if (nameHolder) {
+  nameHolder.style.overflow = "hidden";
+  nameHolder.style.height = `${height}px`;
 }
 
 //getting the acutal name
 const nameIn = nameHolder?.innerText;
-//ascii related variables for 
+//ascii related variables for
 const ASCII_START_CAPITAL = 65;
 const ASCII_START_SMALL = 97;
 const BASE_LOOP_TIMES = 1;
 
 //see if the user has reduced animations
-const query = window.matchMedia('(prefers-reduced-motion: reduce)');
+const query = window.matchMedia("(prefers-reduced-motion: reduce)");
 
 const nameLength = nameIn?.length || 0;
 
 //creating a vatiable to store all the in betwwen name
-let newName:Array<string> = [];
-newName.fill('a',0,nameIn?.length);
-
+let newName: Array<string> = [];
+newName.fill("a", 0, nameIn?.length);
 
 // using a pomise to create a sleep function
 let sleep = (ms: number) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 // calculation for
 let iterations = 0;
-let flag = 0
-
-
+let flag = 0;
 
 //hack for top level await, could be done in a better way? maybe... but did not do
-if(!query.matches)
-(async()=>{while(1){
+if (!query.matches)
+  (async () => {
+    while (1) {
+      // if(window.innerWidth < 500) return;
 
-    // if(window.innerWidth < 500) return;
+      if (!nameIn) return;
 
-    if(!nameIn) return;
+      for (let i = 0; i < nameLength; i++) {
+        const BOUNDARY = BASE_LOOP_TIMES + i;
 
-    for(let i = 0; i < nameLength; i++){
-        const BOUNDARY = BASE_LOOP_TIMES + i; 
-        
-        if(nameIn.charAt(i) == ' '){
-            newName[i] = nameIn[i];
-            continue;
+        if (nameIn.charAt(i) == " ") {
+          newName[i] = nameIn[i];
+          continue;
         }
 
-
-        if(iterations > BOUNDARY){
-            newName[i] = nameIn[i];
-            nameHolder.innerText = newName.join('');
-            if(i == nameLength - 1) flag = 1 
-            continue;
+        if (iterations > BOUNDARY) {
+          newName[i] = nameIn[i];
+          nameHolder.innerText = newName.join("");
+          if (i == nameLength - 1) flag = 1;
+          continue;
         }
-    
-        if(nameIn[i] == ' ') { 
-            newName[i] = ' '; 
-            continue;
+
+        if (nameIn[i] == " ") {
+          newName[i] = " ";
+          continue;
         }
 
         let startBlock = ASCII_START_SMALL;
-        if(nameIn.charCodeAt(i) >= 65 && nameIn.charCodeAt(i) <= 90) startBlock = ASCII_START_CAPITAL;
-        
-        let code = null;
+        if (nameIn.charCodeAt(i) >= 65 && nameIn.charCodeAt(i) <= 90)
+          startBlock = ASCII_START_CAPITAL;
 
-        do{
-            code = startBlock + Math.floor(Math.random() * 26);
-        }while(code == nameIn.charCodeAt(i))
+        let code = 0;
+
+        do {
+          code = startBlock + Math.floor(Math.random() * 26);
+        } while (code == nameIn.charCodeAt(i));
 
         let newLetter = String.fromCharCode(code);
-        newName[i] = newLetter;     
-        nameHolder.innerText = newName.join('');
+        newName[i] = newLetter;
+        nameHolder.innerText = newName.join("");
+      }
+      iterations += 1;
+      await sleep(50);
+      if (flag == 1) break;
     }
-    iterations += 1;
-    await sleep(50);
-    if(flag == 1) break;
-}})();
-
-
-
+  })();
